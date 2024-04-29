a756ba39ee13e1a36f9219ef7d00184fd347cbb7
Pinkha <empksus@gmail.com>
2024-04-30 00:27:06 +0300

    phonebook

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,196 @@
+import { useEffect, useState } from 'react'
+import {getAll, create, remove, update} from './services/numbers.js'
+import "./main.css"
+
+
+const PersonForm = (props) => {
+return (
+<form>
+  <div>
+  Name: <input 
+    value={props.newName}
+    onChange={props.nameHandler}          
+  />
+</div>
+  <div>
+    Number: <input
+      value={props.newNumber}
+      onChange={props.numberHandler}
+      />
+  </div>
+<div>
+  <button type="submit" onClick={props.addPerson}>add</button>
+</div>
+</form>)
+}
+
+const Filter = (props) => {
+  return(
+    <div>
+      Filter : <input
+        value={props.filter}
+        onChange={props.filterHandler}
+        />
+    </div>
+  )
+}
+
+const DeleteButton = (props) => {
+  return (
+    <button type="submit" onClick={() => props.deletePerson(props.id, props.name)}>delete</button> 
+  )
+}
+
+const Person = (props) => {
+  return (
+  <p>{props.name} {props.number} <DeleteButton deletePerson={props.deletePerson} id={props.id} name={props.name}/></p>
+ )
+}
+
+const Persons = (props) => {
+  return (
+    <div> 
+        {props.persons.map((person) => {
+        if (person.name.toLowerCase().includes(props.filteredName.toLowerCase())){
+          return (
+            <Person key={person.id } name={person.name} number={person.number} deletePerson={props.deletePerson} id={person.id} />
+            )
+        } 
+      })}
+    </div>
+  )
+}
+
+const Notification = ({ message}) => {
+  if (message === null) {
+    return null
+  }
+    return (
+      <div className='person'>
+        {message}
+      </div>
+    )
+}
+
+const ErrorMessage = ({message}) => {
+  if(message === null){
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filteredName, setFilter] = useState('');
+  const [Message, setMessage] = useState(null)
+  const [Error, setError] = useState(null)
+  
+  useEffect(async ()  => {
+      const response = await getAll()
+      setPersons(response)
+    }, [])
+
+  const nameHandler = (event) => {
+    setNewName(event.target.value)
+  }
+  
+  const numberHandler = (event) => {
+    setNewNumber(event.target.value)
+  }
+  
+  const filterHandler = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    if (newNumber.length===0){
+      alert(`Add a number to ${newName} in order to save.`)
+      return
+    }
+
+    const newPerson = {
+      name: newName,
+      number: newNumber
+    }
+    let flag = false
+      persons.forEach(person => {
+        if(person.name === newName){
+          if(window.confirm(`${newName} already exists in the phonebook, replace the old number with the new one?`)){
+            
+            update(newNumber, person.id)
+              .then(response => {
+                console.log(response)
+                const index = persons.findIndex(person => person.name === newName)
+                if (index !== -1){
+                  persons[index].number = newNumber
+                  setPersons(persons);
+                  setNewName("")
+                  setNewNumber("")
+                }
+                })
+              .catch(error => {
+                console.log(error)
+                setError(`Information of ${newName} has already been removed from the system`)
+                setPersons(persons.filter(person => person.name !== newName))
+                setTimeout(() => {
+                  setError(null)
+                },5000)
+              })
+            flag=true
+          }else {
+            flag=true
+          }   
+        }
+      });
+
+
+    if (!flag){
+      create(newPerson)
+      .then(response => {
+        setPersons(persons.concat(response));
+        setNewName("");
+        setNewNumber("");
+        setMessage(`Added ${newPerson.name}`);
+        setTimeout(() =>{
+          setMessage(null)
+        }, 5000)
+        });
+      }
+    }
+
+    const deletePerson = (id, name) => {
+
+      if (window.confirm(`Remove ${name}?`)){
+        remove(id)
+          .then(response => {
+            console.log(response)
+            setPersons(persons.filter(person => person.id !== id))
+          })
+      }
+    }
+  
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filteredName} filterHandler={filterHandler}/>
+      <h3>Add a new number</h3>
+      <Notification message={Message} />
+      <ErrorMessage message={Error} />
+      <PersonForm newName={newName} newNumber={newNumber} nameHandler={nameHandler} numberHandler={numberHandler} addPerson={addPerson} />
+      <h3>Numbers</h3>
+      <Persons persons={persons} filteredName={filteredName} deletePerson={deletePerson} />
+    </div>  
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/services/numbers.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+const url = "http://localhost:3001/persons"
+
+const getAll = async () => {
+    const request = axios.get(url)
+    return request.then(response => response.data)
+  }
+  
+  const create = async (newNumber) => {
+    const request = axios.post(url, newNumber)
+    return request.then(response => response.data)
+  }
+
+  const remove = async (id) => {
+    const request = axios.delete(`${url}/${id}`)
+    return request.then(response => response.data)
+  }
+
+  const update = async(number, id) => {
+    const request = axios.patch(`${url}/${id}`, {
+      number: number 
+    })
+    return request.then(response => response.data)
+  }
+
+  export { 
+    getAll, 
+    create,
+    remove,
+    update
+  }
